e02869d4b7f84c2a2ac450b60d07ad873fd01066
Jason Carver <ut96caarrs@snkmail.com>
2018-01-24 16:37:14 -0800

    pull fix from template: start devnum at 0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ extras_require['dev'] = (
 setup(
     name='eth-rlp',
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version='0.1.0-alpha.1',
+    version='0.1.0-alpha.0',
     description="""eth-rlp: RLP definitions for common Ethereum objects in Python""",
     long_description_markdown_filename='README.md',
     author='Jason Carver',
